6554a50f313c69026104fb172e73236ad4c926e7
rafeekbytatech <abdul.rafeek@lxisoft.com>
2019-12-03 20:36:55 +0530

    calcualate totalrating

--- src/main/java/com/diviso/graeshoppe/repository/UserRatingReviewRepository.java
+++ src/main/java/com/diviso/graeshoppe/repository/UserRatingReviewRepository.java
@@ -2,6 +2,7 @@ package com.diviso.graeshoppe.repository;
 
 import com.diviso.graeshoppe.domain.UserRatingReview;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
@@ -15,5 +16,8 @@ public interface UserRatingReviewRepository extends JpaRepository<UserRatingRevi
 	boolean existsByStoreIdAndUserName(Long storeId, String userName);
 
 	UserRatingReview findByStoreIdAndUserName(Long storeId, String userName);
+	
+	@Query("SELECT COUNT(u) FROM UserRatingReviewRepository  u WHERE u.rating=:rating AND u.storeId = :storeId")
+	public int getCount(@Param("rating")Double rating,@Param("storeId")Long storeId);
 
 }
--- src/main/java/com/diviso/graeshoppe/service/impl/UserRatingReviewServiceImpl.java
+++ src/main/java/com/diviso/graeshoppe/service/impl/UserRatingReviewServiceImpl.java
@@ -1,19 +1,23 @@
 package com.diviso.graeshoppe.service.impl;
 
+import com.diviso.graeshoppe.service.StoreService;
 import com.diviso.graeshoppe.service.UserRatingReviewService;
 import com.diviso.graeshoppe.domain.UserRatingReview;
 import com.diviso.graeshoppe.repository.UserRatingReviewRepository;
 import com.diviso.graeshoppe.repository.search.UserRatingReviewSearchRepository;
+import com.diviso.graeshoppe.service.dto.StoreDTO;
 import com.diviso.graeshoppe.service.dto.UserRatingReviewDTO;
 import com.diviso.graeshoppe.service.mapper.UserRatingReviewMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Optional;
 
 import static org.elasticsearch.index.query.QueryBuilders.*;
@@ -33,6 +37,9 @@ public class UserRatingReviewServiceImpl implements UserRatingReviewService {
 
     private final UserRatingReviewSearchRepository userRatingReviewSearchRepository;
 
+    @Autowired
+   	StoreService storeService;
+    
     public UserRatingReviewServiceImpl(UserRatingReviewRepository userRatingReviewRepository, UserRatingReviewMapper userRatingReviewMapper, UserRatingReviewSearchRepository userRatingReviewSearchRepository) {
         this.userRatingReviewRepository = userRatingReviewRepository;
         this.userRatingReviewMapper = userRatingReviewMapper;
@@ -47,12 +54,15 @@ public class UserRatingReviewServiceImpl implements UserRatingReviewService {
      */
     @Override
     public UserRatingReviewDTO save(UserRatingReviewDTO userRatingReviewDTO) {
+    
     	 log.debug("Request to save UserRatingReview : {}", userRatingReviewDTO);
          UserRatingReview userRatingReview = userRatingReviewMapper.toEntity(userRatingReviewDTO);
          userRatingReview = userRatingReviewRepository.save(userRatingReview);
          UserRatingReviewDTO result1 = userRatingReviewMapper.toDto(userRatingReview);
          userRatingReviewSearchRepository.save(userRatingReview);
-        
+         StoreDTO storeDTO = storeService.findOne(result1.getStoreId()).get();
+          Double totalRating=  calculateTotalRating( result1.getStoreId());
+         StoreDTO dto = storeService.save(storeDTO);
          userRatingReview = userRatingReviewMapper.toEntity(result1);
          userRatingReview = userRatingReviewRepository.save(userRatingReview);
         
@@ -124,5 +134,24 @@ public class UserRatingReviewServiceImpl implements UserRatingReviewService {
 	@Override
 	public Long findUserRatingReviwIdByStoreIdAndUserName(Long storeId, String userName) {
 		 return userRatingReviewRepository.findByStoreIdAndUserName(storeId, userName).getId();
+	}
+	private Double calculateTotalRating(Long storeId) {
+		Integer fiveCount = 0, fourCount = 0, threeCount = 0, twoCount = 0, oneCount = 0;
+		fiveCount = userRatingReviewRepository.getCount(5.0,storeId);
+		log.debug(">>>>>>>>>>>>>>>>>5>>>>>>>>>>>>>>>>>>" + fiveCount);
+		fourCount = userRatingReviewRepository.getCount(4.0,storeId);
+		log.debug(">>>>>>>>>>>>>>>>>4>>>>>>>>>>>>>>>>>>" + fourCount);
+		threeCount =userRatingReviewRepository.getCount(3.0,storeId);
+		log.debug(">>>>>>>>>>>>>>>>3>>>>>>>>>>>>>" + threeCount);
+		twoCount = userRatingReviewRepository.getCount(2.0,storeId);
+		log.debug(">>>>>>>>>>>>>>2>>>>>>>>>>>>>>>>>" + twoCount);
+		oneCount = userRatingReviewRepository.getCount(1.0,storeId);
+		log.debug(">>>>>>>>>>>>>>1>>>>>>>>>>>>>>>>>" + oneCount);
+		double rating = (5.0 * fiveCount + 4.0 * fourCount + 3.0 * threeCount + 2.0 * twoCount + 1.0 * oneCount)
+				/ (fiveCount + fourCount + threeCount + twoCount + oneCount);
+		BigDecimal ratingDB = new BigDecimal(rating).setScale(3, RoundingMode.HALF_UP);
+		
+		return ratingDB.doubleValue();
+
 	}
 }
